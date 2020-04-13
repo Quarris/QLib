c49fd9b5c533ef2424fc6c0663d4d3f23d08455f
Quarris <tomasz.winnicki@live.co.uk>
2020-04-13 19:24:33 +0100

    Initial commit

---
+++ src/main/java/quarris/qlib/QLib.java
@@ -0,0 +1,15 @@
+package quarris.qlib;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("qlib")
+public class QLib {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public QLib() {
+
+    }
+}
