8de33c520ab79b4d8635fe4bb9c100bdc001967c
Quarris <tomasz.winnicki@live.co.uk>
2020-04-14 18:44:22 +0100

    Fully remove initial testing grounds

---
+++ src/main/java/quarris/qlib/util/Utils.java
@@ -0,0 +1,15 @@
+package quarris.qlib.util;
+
+import net.minecraft.util.Direction;
+
+public class Utils {
+
+    public static Direction getDirectionFromNormVec(int x, int y, int z) {
+        int val = Math.abs(y) * (    (y+1)/2)
+                + Math.abs(z) * (2 + (z+1)/2)
+                + Math.abs(x) * (4 + (x+1)/2);
+
+        return Direction.byIndex(val);
+    }
+
+}
