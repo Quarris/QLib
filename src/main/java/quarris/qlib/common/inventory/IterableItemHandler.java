4e0c71bc2ae250c4c6cdd17dbe6b26d8c1478ad2
Quarris <tomasz.winnicki@live.co.uk>
2020-04-16 02:00:52 +0100

    Added a wrapper for IItemHandler to make it Iterable

---
+++ src/main/java/quarris/qlib/common/inventory/IterableItemHandler.java
@@ -0,0 +1,35 @@
+package quarris.qlib.common.inventory;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+
+import java.util.Iterator;
+
+public class IterableItemHandler implements Iterable<ItemStack> {
+
+    private IItemHandler itemHandler;
+
+
+    public IterableItemHandler(IItemHandler itemHandler) {
+        this.itemHandler = itemHandler;
+    }
+
+    @Override
+    public Iterator<ItemStack> iterator() {
+        return new Ite();
+    }
+
+    private class Ite implements Iterator<ItemStack> {
+        private int index;
+
+        @Override
+        public boolean hasNext() {
+            return this.index != IterableItemHandler.this.itemHandler.getSlots();
+        }
+
+        @Override
+        public ItemStack next() {
+            return IterableItemHandler.this.itemHandler.getStackInSlot(this.index++);
+        }
+    }
+}
